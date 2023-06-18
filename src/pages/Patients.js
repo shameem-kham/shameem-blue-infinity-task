34f99c243222421672bfc93d6cfb6262f499b3e7
Shameem khan <65350316+shameem-kham@users.noreply.github.com>
2023-06-18 15:21:44 +0530

    hostpital doctor appoinment task
    this site i created using the react js

---
+++ src/pages/Patients.js
@@ -0,0 +1,265 @@
+import "./Patients.css";
+const Patients = () => {
+  return (
+    <div className="patients">
+      <div className="group-parent">
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <div className="rectangle-wrapper">
+            <div className="frame-child" />
+          </div>
+        </div>
+        <div className="instance-child" />
+        <img className="instance-item" alt="" src="/group-148.svg" />
+        <div className="group-container">
+          <div className="group-wrapper">
+            <div className="rectangle-parent">
+              <div className="dashboard">Dashboard</div>
+              <img className="group-item" alt="" src="/group-5.svg" />
+            </div>
+          </div>
+          <div className="patient-parent">
+            <div className="patient">Patient</div>
+            <img className="group-inner" alt="" src="/group-3.svg" />
+          </div>
+          <div className="appointment-parent">
+            <div className="appointment">Appointment</div>
+            <img className="vector-icon" alt="" src="/vector.svg" />
+          </div>
+          <div className="billing-wrapper">
+            <div className="billing">Billing</div>
+          </div>
+        </div>
+        <b className="logo">LOGO</b>
+        <div className="instance-inner" />
+        <div className="group-div">
+          <div className="support-wrapper">
+            <div className="billing">Support</div>
+          </div>
+          <img className="vector-icon1" alt="" src="/vector1.svg" />
+        </div>
+        <div className="group-parent1">
+          <div className="setting-wrapper">
+            <div className="billing">Setting</div>
+          </div>
+          <img className="group-icon" alt="" src="/group.svg" />
+        </div>
+      </div>
+      <div className="group-parent">
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <div className="rectangle-wrapper">
+            <div className="frame-child" />
+          </div>
+        </div>
+        <div className="instance-child" />
+        <img className="instance-item" alt="" src="/group-148.svg" />
+        <div className="group-container">
+          <div className="group-wrapper">
+            <div className="rectangle-parent">
+              <div className="dashboard">Dashboard</div>
+              <img className="group-item" alt="" src="/group-5.svg" />
+            </div>
+          </div>
+          <div className="patient-parent">
+            <div className="patient">Patient</div>
+            <img className="group-inner" alt="" src="/group-3.svg" />
+          </div>
+          <div className="appointment-parent">
+            <div className="appointment">Appointment</div>
+            <img className="vector-icon" alt="" src="/vector.svg" />
+          </div>
+          <div className="billing-wrapper">
+            <div className="billing">Billing</div>
+          </div>
+        </div>
+        <b className="logo">LOGO</b>
+        <div className="instance-inner" />
+        <div className="group-div">
+          <div className="support-wrapper">
+            <div className="billing">Support</div>
+          </div>
+          <img className="vector-icon1" alt="" src="/vector1.svg" />
+        </div>
+        <div className="group-parent1">
+          <div className="setting-wrapper">
+            <div className="billing">Setting</div>
+          </div>
+          <img className="group-icon" alt="" src="/group.svg" />
+        </div>
+      </div>
+      <div className="rectangle-parent1">
+        <div className="group-child3" />
+        <div className="february-2021-parent">
+          <div className="february-2021">February 2021</div>
+          <img className="group-child4" alt="" src="/group-36.svg" />
+          <img className="group-child5" alt="" src="/group-32.svg" />
+        </div>
+      </div>
+      <b className="billing-overview">Billing Overview</b>
+      <div className="patients-child" />
+      <div className="rectangle-parent2">
+        <div className="group-child6" />
+        <div className="search-patients">Search Patients</div>
+        <img className="vector-icon4" alt="" src="/vector2.svg" />
+      </div>
+      <div className="component-3">
+        <div className="children">Add Patient</div>
+      </div>
+      <div className="component-2">
+        <div className="children">Make an Appointment</div>
+      </div>
+      <img className="patients-item" alt="" src="/group-228.svg" />
+      <div className="group-parent6">
+        <div className="rectangle-parent">
+          <div className="rectangle-parent">
+            <div className="group-child7" />
+            <div className="sourav-singh-wrapper">
+              <div className="sourav-singh">Sourav Singh</div>
+            </div>
+            <div className="group-wrapper2">
+              <div className="rectangle-parent">
+                <div className="bill-no">Bill No</div>
+                <div className="div">012345678</div>
+              </div>
+            </div>
+            <div className="male">32, Male</div>
+          </div>
+        </div>
+        <img className="rectangle-icon" alt="" src="/rectangle-39@2x.png" />
+      </div>
+      <div className="group-parent7">
+        <div className="rectangle-parent">
+          <div className="rectangle-parent">
+            <div className="group-child7" />
+            <div className="sourav-singh-wrapper">
+              <div className="sourav-singh">Sourav Singh</div>
+            </div>
+            <div className="group-wrapper2">
+              <div className="rectangle-parent">
+                <div className="bill-no">Bill No</div>
+                <div className="div">012345678</div>
+              </div>
+            </div>
+            <div className="male">32, Male</div>
+          </div>
+        </div>
+        <img className="rectangle-icon" alt="" src="/rectangle-39@2x.png" />
+      </div>
+      <div className="group-parent8">
+        <div className="rectangle-parent">
+          <div className="rectangle-parent">
+            <div className="group-child7" />
+            <div className="sourav-singh-wrapper">
+              <div className="sourav-singh">Sourav Singh</div>
+            </div>
+            <div className="group-wrapper2">
+              <div className="rectangle-parent">
+                <div className="bill-no">Bill No</div>
+                <div className="div">012345678</div>
+              </div>
+            </div>
+            <div className="male">32, Male</div>
+          </div>
+        </div>
+        <img className="rectangle-icon" alt="" src="/rectangle-39@2x.png" />
+      </div>
+      <div className="group-parent9">
+        <div className="rectangle-parent">
+          <div className="rectangle-parent">
+            <div className="group-child7" />
+            <div className="sourav-singh-wrapper">
+              <div className="sourav-singh">Sourav Singh</div>
+            </div>
+            <div className="group-wrapper2">
+              <div className="rectangle-parent">
+                <div className="bill-no">Bill No</div>
+                <div className="div">012345678</div>
+              </div>
+            </div>
+            <div className="male">32, Male</div>
+          </div>
+        </div>
+        <img className="rectangle-icon" alt="" src="/rectangle-39@2x.png" />
+      </div>
+      <div className="patients-inner" />
+      <div className="group-parent10">
+        <div className="rectangle-parent">
+          <div className="rectangle-parent">
+            <div className="group-child11" />
+            <div className="sourav-singh-wrapper2">
+              <div className="sourav-singh">Sourav Singh</div>
+            </div>
+          </div>
+          <div className="male4">32, Male</div>
+        </div>
+        <img className="instance-child6" alt="" src="/rectangle-391@2x.png" />
+        <div className="component-1">
+          <div className="children">View Prescription</div>
+        </div>
+      </div>
+      <div className="patients-child1" />
+      <div className="patients-child2" />
+      <div className="patient-name-parent">
+        <div className="patient-name">Patient Name</div>
+        <div className="children">Sourav Singh</div>
+      </div>
+      <div className="agegender-parent">
+        <div className="patient-name">Age/Gender</div>
+        <div className="children">20/ Male</div>
+      </div>
+      <div className="frame-div">
+        <div className="bill-no-wrapper">
+          <div className="bill-no4">{`Bill No `}</div>
+        </div>
+        <div className="children">01239844</div>
+      </div>
+      <div className="receipt-no-parent">
+        <div className="patient-name">Receipt No</div>
+        <div className="children">0123141125514</div>
+      </div>
+      <div className="date-time-parent">
+        <div className="date-time">Date/ Time</div>
+        <div className="pm">02/02/2023, 06:23 PM</div>
+      </div>
+      <b className="service-name">Service Name</b>
+      <b className="srno">{`Sr.no `}</b>
+      <div className="div6">1</div>
+      <div className="div7">2</div>
+      <div className="consultation">{`Consultation `}</div>
+      <div className="xray">XRay</div>
+      <div className="price-parent">
+        <b className="price">Price</b>
+        <div className="div8">500</div>
+        <div className="div9">1500</div>
+      </div>
+      <div className="quantity-parent">
+        <b className="price">Quantity</b>
+        <div className="div10">1</div>
+        <div className="div11">2</div>
+      </div>
+      <div className="amount-parent">
+        <b className="price">Amount</b>
+        <div className="div12">500</div>
+        <div className="div13">3000</div>
+      </div>
+      <b className="billing-details">Billing Details</b>
+      <div className="component-11">
+        <div className="children">Print Bill</div>
+      </div>
+      <div className="patients-child3" />
+      <div className="patients-child4" />
+      <div className="mobile-number">{`Mobile Number `}</div>
+      <div className="tax">Tax</div>
+      <div className="discount">Discount</div>
+      <b className="total">Total</b>
+      <div className="div14">9327167743</div>
+      <div className="div15">200</div>
+      <div className="div16">50</div>
+      <div className="div17">9327167743</div>
+      <div className="patients-child5" />
+    </div>
+  );
+};
+
+export default Patients;
